64a70c66ff5a017d0cafa910a38b14b151f52e6d
Jeff Tian <jefftian@hcdlearning.com>
2017-12-04 18:11:37 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const Koa = require('koa');
+const app = new Koa();
+const request = require('request');
+const router = require('koa-router')();
+var bodyParser = require('koa-body-parser');
+
+app.use(bodyParser());
+
+router
+    .get('/healthcheck', async ctx => {
+        ctx.body = 'everything is ok';
+    })
+    .post('/', async ctx => {
+        ctx.body = await request(ctx.request.body);
+    });
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
+
+let port = process.env.PORT || 9999;
+app.listen(port);
